0412c49cfd7a3f7f7badc89694275f04c6711c60
maurelio631 <marco.silva631@outlook.com>
2023-01-31 09:40:51 -0300

    Criando e aplicando função de normalização da porta à ser escutada pelo servidor

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 const debug = require("debug")("ApiNode:server");
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = normalizePort(process.env.PORT || "3000");
 const server = http.createServer(app);
 const router = express.Router();
 const route = router.get("/", (req, res, next) => {
@@ -20,3 +20,16 @@ app.use("/", route);
 server.listen(PORT);
 
 console.log(`Servidor rodando e ouvindo à porta ${PORT}`);
+
+function normalizePort(val) {
+  const port = parseInt(val);
+
+  if (isNaN) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
