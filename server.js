d2ca60acba2b25875ec300eca6ca7f84894ac789
maurelio631 <marco.silva631@outlook.com>
2023-01-31 09:34:07 -0300

    Importação de dependências, criação da porta para escutar aos requests e o servidor já funciona basicamente

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log("teste");
+const http = require("http");
+const debug = require("debug")("ApiNode:server");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const server = http.createServer(app);
+const router = express.Router();
+const route = router.get("/", (req, res, next) => {
+  res.status(200).send({
+    title: "API Node",
+    version: "0.0.1",
+  });
+});
+
+app.set("port", PORT);
+app.use("/", route);
+
+server.listen(PORT);
+
+console.log(`Servidor rodando e ouvindo à porta ${PORT}`);
