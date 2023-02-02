cde7a77c02fbd2ff045d6b2db19bc7f4465383dc
maurelio631 <marco.silva631@outlook.com>
2023-02-02 11:02:04 -0300

    Preparação do método de post e persistindo dados no banco de maneira correta com a validação do model

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
+
 const mongoose = require("mongoose");
+mongoose.connect("mongodb://marco:mongoDev@localhost:27017/?authSource=admin");
+const Product = require("./models/product");
+
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/product-route");
 
-mongoose.connect("mongodb://marco:mongoDev@localhost:27017/?authSource=admin");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
