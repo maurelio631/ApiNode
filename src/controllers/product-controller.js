024ad4148cc5b1a3e47aeaa5c8cb9c46f73c4a5a
maurelio631 <marco.silva631@outlook.com>
2023-02-03 10:43:57 -0300

    Criando classe de validadores e aplicando validação no método post

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,7 @@
 
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
+const ValidationContract = require("../validators/fluent-validator");
 
 exports.get = (req, res, next) => {
   Product.find({ active: true }, "title price slug")
@@ -37,7 +38,10 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-  Product.find({ tags: req.params.tag, active: true }, 'title description price slug tags')
+  Product.find(
+    { tags: req.params.tag, active: true },
+    "title description price slug tags"
+  )
     .then((data) => {
       res.status(200).send(data);
     })
@@ -47,6 +51,28 @@ exports.getByTag = (req, res, next) => {
 };
 
 exports.post = (req, res, next) => {
+  let contract = new ValidationContract();
+  contract.hasMinLen(
+    req.body.title,
+    3,
+    "O título do produto deve conter pelo menos 3 caracteres"
+  );
+  contract.hasMinLen(
+    req.body.slug,
+    3,
+    "O slug do produto deve conter pelo menos 3 caracteres"
+  );
+  contract.hasMinLen(
+    req.body.slug,
+    10,
+    "A descrição do produto deve conter pelo menos 10 caracteres"
+  );
+
+  if (!contract.isValid()) {
+    res.status(400).send(contract.errors()).end();
+    return;
+  }
+
   var product = new Product(req.body);
   product
     .save()
@@ -61,22 +87,24 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-  Product.findByIdAndUpdate(req.params.id,{
-    $set:{
+  Product.findByIdAndUpdate(req.params.id, {
+    $set: {
       title: req.body.title,
       description: req.body.description,
-      price:req.body.price
-    }
-  }).then(x=>{
-    res.status(200).send({
-      message:"Produto atualizado com sucesso!"
-    })
-  }).catch(e=>{
-    res.status(400).send({
-      message:'Falha ao atualizar o produto',
-      data: e
-    })
+      price: req.body.price,
+    },
   })
+    .then((x) => {
+      res.status(200).send({
+        message: "Produto atualizado com sucesso!",
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: "Falha ao atualizar o produto",
+        data: e,
+      });
+    });
 };
 
 exports.delete = (req, res, next) => {
