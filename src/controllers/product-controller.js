b98c2289bd44450ceec521b353030ec7632f2b72
maurelio631 <marco.silva631@outlook.com>
2023-02-03 21:47:13 -0300

    Metodo de delete implementado e aplicando o repository pattern

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,10 +3,11 @@
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
 const ValidationContract = require("../validators/fluent-validator");
+const repository = require('../repositories/product-repository')
+
 
 exports.get = (req, res, next) => {
-  Product.find({ active: true }, "title price slug")
-    .then((data) => {
+  repository.get().then((data) => {
       res.status(200).send(data);
     })
     .catch((e) => {
@@ -15,10 +16,7 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-  Product.findOne(
-    { slug: req.params.slug, active: true },
-    "title description price slug tags"
-  )
+  repository.getBySlug(req.params.slug)
     .then((data) => {
       res.status(200).send(data);
     })
@@ -28,7 +26,7 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-  Product.findById(req.params.id)
+  repository.getByid(req.params.id)
     .then((data) => {
       res.status(200).send(data);
     })
@@ -38,10 +36,7 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-  Product.find(
-    { tags: req.params.tag, active: true },
-    "title description price slug tags"
-  )
+  repository.getByTag(req.params.tag)
     .then((data) => {
       res.status(200).send(data);
     })
@@ -108,6 +103,16 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send(req.body);
+  Product.findOneAndRemove(req.body.id)
+    .then((x) => {
+      res.status(200).send({
+        message: "Produto removido com sucesso!",
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: "Falha ao remover o produto",
+        data: e,
+      });
+    });
 };
