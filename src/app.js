187ed4b9ccb28090956aea2a8dc898c4f4289e12
maurelio631 <marco.silva631@outlook.com>
2023-02-06 09:35:47 -0300

    Modelo order criado

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://marco:mongoDev@localhost:27017/?authSource=admin");
 const Product = require("./models/product");
 const Customer = require("./models/customer");
+const Order = require("./models/order");
 
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/product-route");
